c8bf959464f94323dd3b2efc10f6a5f4803d4666
曹智铭 <xiaocao162020@xiaocao.onmicrosoft.com>
2023-11-20 11:53:18 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "曹智铭的云盘",
+  title: "ClaudiaCloud",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/cao_cloud',
@@ -35,7 +35,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    '曹智铭的云盘，曹智铭。',
+    'Claudia Cloud, made by Cao Zhiming.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
